53a545b5762227d298e1954c6ba25494cef77e3c
Chandan Kumar Maurya <131549483+ichanddan@users.noreply.github.com>
2024-08-03 16:12:31 +0530

    Update case_creation.routes.js

--- Routes/case_creation.routes.js
+++ Routes/case_creation.routes.js
@@ -1,5 +1,5 @@
 import Express  from "express";
-import { caseCreation, caseCreationUpload, caseDocument, getCase_List_By_id, getCase_d_by_case_id } from "../controllers/case_creation.controllers.js";
+import { DocumentUpdate, caseCreation, caseCreationUpload, caseDocument, getCase_List_By_id, getCase_d_by_case_id } from "../controllers/case_creation.controllers.js";
 import { verifyToken } from "../Middleware/jwt.auth.js";
 import { uploadMulti } from "../Middleware/Upload.js";
 const Case_Creation= Express.Router()
@@ -8,6 +8,7 @@ const Case_Creation= Express.Router()
 Case_Creation.post("/cas_creation",verifyToken,caseCreation)
 Case_Creation.post("/cas_creation/upload", uploadMulti, caseCreationUpload)
 Case_Creation.get("/cas_document", caseDocument)
+Case_Creation.post("/cas_creation/update", DocumentUpdate)
 Case_Creation.get("/cas_document/list_of_case/:user_id",verifyToken ,getCase_List_By_id);
 Case_Creation.get("/cas_document/d_of_case/:case_id", getCase_d_by_case_id);
 
